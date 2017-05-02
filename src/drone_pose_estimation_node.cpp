2f36995a2b3536da4f4490e4101f8c25ce2c7718
Mohsen ThinkPad <mohsen.mirhassani@gmail.com>
2017-05-02 16:21:21 +0200

    code tested and working

--- src/drone_pose_estimation_node.cpp
+++ src/drone_pose_estimation_node.cpp
@@ -1,3 +1,21 @@
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+#define KLGRY "\x1b[90m"
+#define KLRED "\x1b[91m"
+#define KLGRN "\x1b[92m"
+#define KLYEL "\x1b[93m"
+#define KLBLU "\x1b[94m"
+#define KLMAG "\x1b[95m"
+#define KLCYN "\x1b[96m"
+#define KLWHT "\x1b[97m"
+
+
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
@@ -13,12 +31,17 @@ struct PARAM
   std::string Body_frame;
   std::string drone_frame_id;
   bool use_tf;
+  bool debug;
+  bool test;
+  bool send_tf;
   double delay; // in sec
   double hovering_gain;
   double cam_gain;
-  void init()
+  double test_omega;
+  void init(ros::NodeHandle& np)
   {
-    ros::NodeHandle np("~");
+//    ros::NodeHandle np("~");
+    //Default values
     campose_topic_name = "/uav_pose_from_cam";
     outpose_topic_name = "/uav_pose_filtered";
     IF_frame = "/map";
@@ -28,25 +51,42 @@ struct PARAM
     hovering_gain = 5.0;
     cam_gain = 5.0;
     drone_frame_id = "/uav";
-    np.param("campose_topic_name",campose_topic_name);
+    debug = false;
+    test = true;
+    test_omega = 0.4;
+    send_tf = false;
+
+    np.getParam("campose_topic_name",campose_topic_name);
     ROS_INFO_STREAM("campose_topic_name: " << campose_topic_name);
-    np.param("output_pose_topic_name",outpose_topic_name);
+    np.getParam("output_pose_topic_name",outpose_topic_name);
     ROS_INFO_STREAM("output_pose_topic_name: " << outpose_topic_name);
-    np.param("Inertial_frame_id",IF_frame);
+    np.getParam("Inertial_frame_id",IF_frame);
     ROS_INFO_STREAM("Inertial_frame_id: " << IF_frame);
-    np.param("Cam_frame_id",Body_frame);
+    np.getParam("Cam_frame_id",Body_frame);
     ROS_INFO_STREAM("Cam_frame_id: " << Body_frame);
-    np.param("use_tf_for_cam_pose",use_tf);
+    np.getParam("use_tf_for_cam_pose",use_tf);
     ROS_INFO_STREAM("use_tf_for_cam_pose: " << use_tf);
-    np.param("total_delay",delay,0.1);
+    np.getParam("total_delay",delay);
     ROS_INFO_STREAM("total_delay: " << delay << " seconds");
-    np.param("hovering_gain",hovering_gain);
+    np.getParam("hovering_gain",hovering_gain);
     ROS_INFO_STREAM("hovering_gain: " << hovering_gain);
-    np.param("cam_gain",cam_gain);
+    np.getParam("send_tf",send_tf);
+    if(send_tf) ROS_INFO_STREAM("sending TF  /" << Body_frame << "  to  /uav_filtered");
+    np.getParam("cam_gain",cam_gain);
     ROS_INFO_STREAM("cam_gain: " << cam_gain);
+    np.getParam("test",test);
+    if(test)
+    {
+      np.getParam("test_omega",test_omega);
+      ROS_WARN_STREAM("Test mode, a fake circle transform will be published to test the code angular speed: "
+                      << test_omega);
+
+    }
+    np.getParam("debug",debug);
+    ROS_WARN_COND(debug,"Log mode: debug!");
     if(use_tf)
     {
-      np.param("drone_frame_id",drone_frame_id);
+      np.getParam("drone_frame_id",drone_frame_id);
       ROS_INFO_STREAM("drone_frame_id: " << drone_frame_id);
     }
   }
@@ -58,6 +98,8 @@ class DroneFilter
 
   DroneFilter(ros::NodeHandle& node)
   {
+     ros::NodeHandle n("~");
+     params.init(n);
      nh_ = node;
      //Subscribers
      subFromcam_ = nh_.subscribe(params.campose_topic_name, 1, &DroneFilter::camcb, this);
@@ -66,19 +108,20 @@ class DroneFilter
      IF_pose_publisher = nh_.advertise<geometry_msgs::Pose>(params.outpose_topic_name,1);
 
      init_att();
-     params.init();
+
   }
 
   void init_att() //initialize the class attributes
   {
     pose_from_cam.new_msg = false;
     first_data_received = false;
-    rate = 10.0;
+    rate = 200.0;
+
   }
 
   void camcb(const geometry_msgs::PoseStamped::ConstPtr& msg)
   {
-    ROS_INFO("new pose received");
+    ROS_INFO_COND(params.debug,"new pose received");
     pose_from_cam.update(*msg);
     if(!first_data_received) {
       ROS_INFO("Cam Measurment Received!");
@@ -86,42 +129,114 @@ class DroneFilter
     }
   }
 
+  void test_circle_tf(tf::TransformBroadcaster& br,double theta)
+  {
+    double r = 1.5;
+
+    geometry_msgs::TransformStamped trans;
+    trans.header.stamp = ros::Time::now();
+    trans.header.frame_id = params.IF_frame;
+    trans.child_frame_id = params.Body_frame;
+    trans.transform.translation.x = r*cos(theta);
+    trans.transform.translation.y = r*sin(theta);
+    trans.transform.translation.z = 0;
+    trans.transform.rotation.w = 1;
+    br.sendTransform(trans);
+//    tf::Transform trans;
+//    trans.setOrigin(tf::Vector3(r*cos(theta),r*sin(theta),0.0));
+//    trans.setRotation(tf::Quaternion(0.0,0.0,0.0,1.0));
+//    br.sendTransform(tf::StampedTransform(trans,ros::Time::now(),params.IF_frame,params.Body_frame));
+  }
+
+  void send_tf(tf::TransformBroadcaster& br,geometry_msgs::Pose msg)
+  {
+    geometry_msgs::TransformStamped trans;
+    trans.header.stamp = ros::Time::now();
+    trans.header.frame_id = params.IF_frame;
+    trans.child_frame_id = "/uav_filtered";
+    trans.transform.translation.x = msg.position.x;
+    trans.transform.translation.y = msg.position.y;
+    trans.transform.translation.z = msg.position.z;
+    trans.transform.rotation.w = msg.orientation.w;
+    trans.transform.rotation.x = msg.orientation.x;
+    trans.transform.rotation.y = msg.orientation.y;
+    trans.transform.rotation.z = msg.orientation.z;
+    br.sendTransform(trans);
+  }
+
   void handle()
   {
 
      ros::Rate r(rate);
-     tf::TransformListener Body_IF_Listener,  //Inertial Frame to Body Frame
+     tf::TransformListener IF_Body_Listener,  //Inertial Frame to Body Frame
                            cam_goal_Listener;
-     tf::StampedTransform  Body_IF_Trans,
+     tf::StampedTransform  IF_Body_Trans,
                            cam_goal_Trans;
-     Body_IF_Trans.setIdentity(); // initializing
-     if(!Body_IF_Listener.waitForTransform(params.Body_frame,params.IF_frame,ros::Time(0),ros::Duration(10,0)))
-     {
-       ROS_FATAL_STREAM("Transform from " << params.IF_frame << " to " << params.Body_frame << " not found!");
-       exit(0);
-     }
+     tf::TransformBroadcaster br;
+     IF_Body_Trans.setIdentity(); // initializing
+     if(!params.test)
+       if(!IF_Body_Listener.waitForTransform(params.Body_frame,params.IF_frame,ros::Time(0),ros::Duration(10,0)))
+       {
+         ROS_FATAL_STREAM("Transform from " << params.IF_frame << " to " << params.Body_frame << " not found!");
+         exit(0);
+       }
+     double th = 0.0;
+     first_loop = true;
 
      while(nh_.ok())
      {
+       if(params.test)
+       {
+//         ROS_INFO_THROTTLE(0.5," <<<<<<< TEST >>>>>>>");
+         test_circle_tf(br,th);
+         th += params.test_omega/rate;
+       }
+
+       if(params.use_tf)
+       {
+          ROS_INFO_ONCE("getting cam_drone relative pose using TF");
+          try
+          {
+            cam_goal_Listener.lookupTransform(params.Body_frame,params.drone_frame_id,ros::Time(0),cam_goal_Trans);
+            ROS_INFO_COND(params.debug,"new pose received");
+            pose_from_cam.update(cam_goal_Trans);
+
+            if(!first_data_received) {
+              ROS_INFO("Cam Measurment Received!");
+              first_data_received = true;
+            }
+          }
+          catch(tf::TransformException ex)
+          {
+            ROS_WARN("%s",ex.what());
+//            ros::Duration(0.3).sleep();
+          }
+       }
+
        if(first_data_received)
        {
          if(pose_from_cam.new_msg)
          {
            try
            {
-             Body_IF_Listener.lookupTransform(params.Body_frame,params.IF_frame,pose_from_cam.header.stamp - ros::Duration(params.delay),Body_IF_Trans);
-             pose_from_cam.pose_IF = Body_IF_Trans * pose_from_cam.pose_body;
+             IF_Body_Listener.lookupTransform(params.IF_frame,params.Body_frame,/*ros::Time(0)*/pose_from_cam.header.stamp - ros::Duration( params.delay),IF_Body_Trans);
+             pose_from_cam.pose_IF = IF_Body_Trans * pose_from_cam.pose_body;
              pose_from_cam.new_msg = false;
              /* CAM UPDATE */
-             drone_filtered = (params.hovering_gain * drone_0 + params.cam_gain * pose_from_cam.pose_IF.getOrigin()) *
-                 (1/(params.hovering_gain + params.cam_gain));
-
+             if(!first_loop)
+               drone_filtered = (params.hovering_gain * drone_0 + params.cam_gain * pose_from_cam.pose_IF.getOrigin()) *
+                   (1/(params.hovering_gain + params.cam_gain));
+             else
+             {
+               first_loop = false;
+               drone_filtered = pose_from_cam.pose_IF.getOrigin();
+             }
              drone_0 = drone_filtered;
            }
            catch(tf::TransformException ex)
            {
-             ROS_WARN("%s",ex.what());
-             ros::Duration(0.3).sleep();
+             ROS_WARN_COND(params.debug,"%s",ex.what());
+//             ros::Duration(0.3).sleep();
            }
          }
 //         else
@@ -133,32 +248,15 @@ class DroneFilter
          msg.orientation.w = 1.0;  //orientation is not being considered here
 
          IF_pose_publisher.publish(msg);
+         if(params.send_tf) send_tf(br,msg);
        }
-       else if(params.use_tf)
-       {
-          ROS_INFO_ONCE("getting cam_drone relative pose using TF");
-          try
-          {
-            cam_goal_Listener.lookupTransform(params.Body_frame,params.drone_frame_id,ros::Time(0),cam_goal_Trans);
-            ROS_INFO("new pose received");
-            pose_from_cam.update(cam_goal_Trans);
-
-            if(!first_data_received) {
-              ROS_INFO("Cam Measurment Received!");
-              first_data_received = true;
-            }
-          }
-          catch(tf::TransformException ex)
-          {
-            ROS_WARN("%s",ex.what());
-            ros::Duration(0.3).sleep();
-          }
-       }
-       else
+       else if(!params.use_tf)
          ROS_WARN_THROTTLE(1,"Still waiting for the cam measurment");
+
 
        r.sleep();
        ros::spinOnce();
+
      }
   }
 
@@ -169,7 +267,7 @@ protected:
   // Publishers
   ros::Publisher IF_pose_publisher;
   double rate;
-
+  bool first_loop;
 private:
   struct TFPOSESTAMPED{
     tf::Pose pose_body;
@@ -184,8 +282,11 @@ private:
     }
     void update(tf::StampedTransform trans)
     {
+      new_msg = true;
       pose_body.setOrigin(trans.getOrigin());
       pose_body.setRotation(trans.getRotation());
+      header.stamp = trans.stamp_;
+//      ROS_INFO_STREAM(KLGRN<<trans.stamp_);
     }
   }pose_from_cam;
   tf::Vector3 drone_filtered,drone_imu,drone_0;
@@ -196,9 +297,8 @@ private:
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "drone_pose_node");
-  ros::NodeHandle node;
-
-//  tester tt(node);
-//  tt.handle();
+  ros::NodeHandle n;
+  DroneFilter node(n);
+  node.handle();
   return 0;
 }
